88cca5cabcd1fe9af0537702c623ee5160d981ab
p-acid <qkrtkstjd01@gmail.com>
2025-05-19 19:01:38 +0900

    fix: home style

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,23 @@
-import { PAGE_ROUTES } from "@/constants/page-routes";
 import Link from "next/link";
+
+import { PAGE_ROUTES } from "@/constants/page-routes";
 
 export default function Home() {
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center gap-20 px-4">
+    <main className="flex h-full w-full flex-col items-center justify-center gap-20 px-6">
       <div className="flex w-full flex-col gap-5">
         <div className="w-fit rounded-md bg-zinc-800 px-2 py-1 text-2xl font-black">
           漢
         </div>
-        <h1 className="text-5xl leading-10 font-semibold tracking-tighter whitespace-pre">
-          {"HANJA\nRANDOM QUIZ"}
+        <h1 className="mt-2 text-4xl leading-10 font-bold tracking-tighter whitespace-pre">
+          랜덤 한자 퀴즈
         </h1>
-        <p className="text-lg leading-5.5 tracking-tight whitespace-pre text-zinc-400">
-          {"Memorize Chinese Characters\nby Random Quiz"}
+        <p className="text-lg tracking-tight whitespace-pre text-zinc-400">
+          {"약 900개의 한자 단어를\n랜덤 퀴즈를 통해 외워보세요!"}
         </p>
       </div>
       <Link href={PAGE_ROUTES.QUIZ} className="btn w-full">
-        Get Start
+        시작하기
       </Link>
     </main>
   );
