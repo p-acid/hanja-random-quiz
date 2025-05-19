cd4d30032a63d68ab04b580d9287e9acc4b572b0
p-acid <qkrtkstjd01@gmail.com>
2025-05-19 17:26:19 +0900

    chore: pwa default settings

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import { SERVICE_METADATA } from "@/constants/service";
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: SERVICE_METADATA.TITLE,
+    short_name: "HRQ",
+    description: SERVICE_METADATA.DESCRIPTION,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
