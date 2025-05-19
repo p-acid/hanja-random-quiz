9437fa1e598d9edde52614124e39ccfaf90e516b
p-acid <qkrtkstjd01@gmail.com>
2025-05-19 21:08:53 +0900

    fix: mobile size error

--- src/layouts/main-layout/index.tsx
+++ src/layouts/main-layout/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function MainLayout({ children }: PropsWithChildren) {
   return (
-    <div className="bg-base-100 flex h-full min-h-screen w-full justify-center">
+    <div className="bg-base-100 flex h-full min-h-[100dvh] w-full justify-center">
       <div className="w-full max-w-sm">{children}</div>
     </div>
   );
