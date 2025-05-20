daf261afec4f7433a2d45ba364fdf031d3816e3e
p-acid <qkrtkstjd01@gmail.com>
2025-05-20 16:13:43 +0900

    style: empty view

--- src/app/quiz/result/src/ui/submitted-answers.tsx
+++ src/app/quiz/result/src/ui/submitted-answers.tsx
@@ -3,6 +3,7 @@
 import { SubmittedQuiz, submittedQuizzesAtom } from "@/atoms/submitted-quizzes";
 import { ChineseCharacter } from "@/components/chinese-character";
 import { useAtomValue } from "jotai";
+import { CircleSlash2 } from "lucide-react";
 
 export function SubmittedAnswers() {
   const submittedQuizzes = useAtomValue(submittedQuizzesAtom);
@@ -26,30 +27,45 @@ export function SubmittedAnswers() {
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex flex-col gap-5">
-        <h1 className="text-xl font-bold tracking-tighter">맞은 답안</h1>
-        <div className="flex flex-col gap-3">
-          {correctAnswers.map((quiz, index) => (
-            <ChineseCharacter
-              key={index}
-              word={quiz.answer.word}
-              reading={quiz.answer.reading}
-            />
-          ))}
-        </div>
-      </div>
+      <AnswerList
+        title="맞은 답안"
+        answers={correctAnswers}
+        emptyMessage="맞은 답안이 없습니다."
+      />
+      <AnswerList
+        title="틀린 답안"
+        answers={incorrectAnswers}
+        emptyMessage="틀린 답안이 없습니다."
+      />
+    </div>
+  );
+}
 
-      <div className="flex flex-col gap-5">
-        <h1 className="text-xl font-bold tracking-tighter">틀린 답안</h1>
-        <div className="flex flex-col gap-3">
-          {incorrectAnswers.map((quiz, index) => (
+interface AnswerListProps {
+  title: string;
+  answers: SubmittedQuiz[];
+  emptyMessage: string;
+}
+
+function AnswerList({ title, answers, emptyMessage }: AnswerListProps) {
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-xl font-bold tracking-tighter">{title}</h1>
+      <div className="flex flex-col gap-3">
+        {answers.length === 0 ? (
+          <div className="bg-base-200 flex h-40 flex-col items-center justify-center gap-5 rounded-lg">
+            <CircleSlash2 className="text-base-content size-10" />
+            <span className="text-zinc-400">{emptyMessage}</span>
+          </div>
+        ) : (
+          answers.map((quiz, index) => (
             <ChineseCharacter
               key={index}
               word={quiz.answer.word}
               reading={quiz.answer.reading}
             />
-          ))}
-        </div>
+          ))
+        )}
       </div>
     </div>
   );
