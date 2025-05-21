94f8f51f2bc3b60bd7b3b770f8a54238327a6a18
p-acid <qkrtkstjd01@gmail.com>
2025-05-21 22:08:50 +0900

    style: og image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -18,19 +18,40 @@ export default async function Image() {
           width: "100%",
           height: "100%",
           background: "black",
+          gap: "12px",
         }}
       >
         <h1
           style={{
-            fontSize: 30,
+            fontSize: 120,
             lineHeight: 1.2,
-            fontWeight: "bold",
-            letterSpacing: "-0.05em",
+            fontWeight: "black",
+            letterSpacing: "-0.15em",
             color: "white",
           }}
         >
-          한랜퀴🐼
+          랜덤 한자 퀴즈
         </h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: 120,
+            lineHeight: 1.2,
+            fontWeight: "black",
+            color: "white",
+            width: "170px",
+            height: "170px",
+            paddingRight: "12px",
+            paddingBottom: "12px",
+            borderRadius: "16px",
+            background: "#2b2b2b",
+            transform: "rotate(6deg) translateY(12px)",
+          }}
+        >
+          漢
+        </div>
       </div>
     ),
     {
